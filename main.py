3606aec1733d0d3eaa6cb1420431a57169c34999
Matias Montenegro <matiasmontenegro0396@gmail.com>
2021-11-13 12:08:57 -0300

    Agregue funciones de administrador y comentarios a funciones

--- main.py
+++ main.py
@@ -21,22 +21,25 @@ import json
 f = open("productos.json")
 data = json.load(f)
 
-#close the original file
+#close el archivo original
 f.close()
 
-#Hay que usar la funcion "Dump" para actualizar el archivo json
-# json_object["d"] = 100 -> aca se actualiza el "D" al nuevo valor 100
-# a_file = open("sample_file.json", "w") -> se abre el archivo devuelta, pero ahora en modo "write"
-# json.dump(json_object, a_file) -> aca se usa dump, el primer argumento es el objecto json que ya teniamos, 
+# Hay que usar la funcion "Dump" para actualizar el archivo json
+#  json_object["d"] = 100 -> aca se actualiza el "D" al nuevo valor 100
+#  a_file = open("sample_file.json", "w") -> se abre el archivo devuelta, pero ahora en modo "write"
+#  json.dump(json_object, a_file) -> aca se usa dump, el primer argumento es el objecto json que ya teniamos, 
 #                                   y el segundo argumento es el archivo que abrimos devuelta pero con write
-# a_file.close() -> se cierra el archivo despues de actualizar
+#  a_file.close() -> se cierra el archivo despues de actualizar
 
 
-#Actualizar archivo de los productos
 
 # ---------------------------- Global functions ----------------------------------------
 def precio(dicc, producto):
-
+    ''' Devuelve el precio del producto seleccionado
+    '''
+ 
+ 
+ 
  #Falta agregar que pasa si el producto no esta en el dicc
  # o tal vez de eso se encarga otra funcion, lo que haria (creo) 
  # que todo lo de abajo no sea necesario
@@ -51,6 +54,10 @@ def precio(dicc, producto):
 
 def mostrarProductos(dicc):
 
+    """ Muestra TODOS los productos dentro del archivo, incluidos los precios y stock
+    """
+
+
 
     for producto in dicc:
 
@@ -59,6 +66,10 @@ def mostrarProductos(dicc):
 
 def sacarStock(dicc, producto, cantidad):
     
+    """ Saca Stock del producto seleccionado
+    """
+   
+   
     # Abrir para actualizarlo
     f = open("productos.json", "w")
 
@@ -71,12 +82,13 @@ def sacarStock(dicc, producto, cantidad):
 
 
 
-# ---------------------------- User functions ----------------------------------------
+# ---------------------------- User functions ------------------------------------------
 
-
-# Pregunta al usuario si sabe lo que quiere comprar. En caso de que no sepa, muestra la lista de productos
-# en caso de que sepa lo que quiere, se le pregunta el nombre del producto y se agrega al "carrito"
 def comprar(dicc):
+    ''' Pregunta al usuario si sabe lo que quiere comprar. En caso de que no sepa, muestra la lista de productos
+        en caso de que sepa lo que quiere, se le pregunta el nombre del producto y se agrega al "carrito"
+    '''
+    
     
     #terminar variable para que siga el programa hasta que el usuario haga el "checkout"
     terminar = False
@@ -131,5 +143,65 @@ def comprar(dicc):
 
 
 
-# ------------ Testing Program / Functions-------------------------
-comprar(data)
+# ---------------------------- Admin functions ------------------------------------------
+def agregarStock(dicc, producto, cantidad):
+
+    """ Solo cambia la cantidad de stock del producto seleccionado
+    """
+
+    # Abrir para actualizarlo
+    f = open("productos.json", "w")
+
+    dicc[producto][2] = dicc[producto][2] + cantidad
+
+    #actualizo el archivo json y lo cierro.
+    json.dump(dicc, f)
+
+    f.close()
+
+def agregarProducto(dicc, producto, precio, cantidad):
+    
+    ''' Se agrega un producto totalmente nuevo al json. El id es lo que seria el identificador de cada
+    producto si fuera un kiosko real, por eso se necesita que cada id sea diferente. Como estamos 
+    haciendo el diccionario/json de forma ordenada, basta con saber cual es el id del ultimo producto
+    y sumarle 1
+    '''
+
+
+
+    # para agarrar el ultimo elemento del diccionario -> list(dicc)[-1]
+    ultimoProducto = list(dicc)[-1]
+
+    ultimoId = dicc[ultimoProducto][0] + 1 # necesitamos que sea un nuevo id, por eso agrego el +1
+
+    # Abrir para actualizarlo
+    f = open("productos.json", "w")
+
+    # Agrego el nuevo producto al diccionario
+    dicc[producto] = [ultimoId,precio,cantidad]
+
+    #actualizo el archivo json y lo cierro.
+    json.dump(dicc, f)
+
+    f.close()
+
+def sacarProducto(dicc, producto):
+   
+    """ borra el key-value completamente del archivo
+    """
+    
+    # Abrir para actualizarlo
+    f = open("productos.json", "w")
+
+    # Borra el "objeto". En python el key-value es un objeto, y por eso se puede borrar con del
+    # otra forma seria usando .pop -> dicc.pop(producto) 
+    del dicc[producto]
+
+    #actualizo el archivo json y lo cierro.
+    json.dump(dicc, f)
+
+    f.close() 
+
+# ------------ Testing Program / Functions--------------------------
+
+sacarProducto(data, "chicle")
